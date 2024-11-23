265ea96d1ff75d79e81797420ac8017e75ed0709
Rimawi, Danya Hazim <drimawi@iu.edu>
2024-11-23 18:16:15 -0500

    Working and using Spline interpolation

--- Processwithplot/Processwithplot.cpp
+++ Processwithplot/Processwithplot.cpp
@@ -17,7 +17,6 @@ ProcessWithPlot::ProcessWithPlot(QWidget *parent) : QWidget(parent) {
     // Perform FFT and plot the result
     performFFTAndPlot();
 }
-
 void ProcessWithPlot::performFFTAndPlot() {
     // Load data from file
     ifstream file("C:/Users/danya/Downloads/sin_noise_example.csv");
@@ -26,7 +25,7 @@ void ProcessWithPlot::performFFTAndPlot() {
         return;
     }
 
-    vector<double> data;
+    QVector<double> data;
     string line;
     while (getline(file, line)) {
         const auto pos = line.find(',');
@@ -37,57 +36,53 @@ void ProcessWithPlot::performFFTAndPlot() {
     }
     file.close();
 
+
+    QVector<double> calibratedIntensities = data;
+
     // Compute wavelengths
-    const vector<double> c = {645.9716187, 0.15915893, -4.296465E-06, -3.793865E-10};
-    const size_t dataSize = data.size();
-    vector<double> wavelengths(dataSize);
-    for (size_t i = 0; i < dataSize; ++i) {
+    const vector<double> c = { 645.9716187, 0.15915893, -4.296465E-06, -3.793865E-10 };
+    const size_t numPixels = data.size();
+  
+
+    vector<double> wavelengths(numPixels);
+    for (size_t i = 0; i < numPixels; ++i) {
         wavelengths[i] = c[0] + c[1] * i + c[2] * i * i + c[3] * i * i * i;
     }
 
     // Interpolation
-    double xwavmax = ceil(*max_element(wavelengths.begin(), wavelengths.end()));
-    double xwavmin = floor(*min_element(wavelengths.begin(), wavelengths.end()));
+    int interpSize = numPixels * 2; // Double the sampling frequency
+    QVector<double> interpIntensities(interpSize);
+    QVector<double> interpWavelengths(interpSize);
 
-    vector<double> new_x(dataSize);
-    for (size_t i = 0; i < dataSize; ++i) {
-        new_x[i] = static_cast<double>(xwavmin + i * (xwavmax - xwavmin) / (dataSize - 1));
+    for (int i = 0; i < interpSize; ++i) {
+        double t = static_cast<double>(i) / (interpSize - 1);
+        double origIndex = t * (numPixels - 1);
+        int idx = static_cast<int>(origIndex);
+        double frac = origIndex - idx;
+
+        if (idx + 1 < numPixels) {
+            interpIntensities[i] = (1 - frac) * calibratedIntensities[idx] + frac * calibratedIntensities[idx + 1];
+            interpWavelengths[i] = (1 - frac) * wavelengths[idx] + frac * wavelengths[idx + 1];
+        }
+        else {
+            interpIntensities[i] = calibratedIntensities[idx];
+            interpWavelengths[i] = wavelengths[idx];
+        }
     }
 
-    auto interpolate = [](const vector<double> &x, const vector<double> &y, const vector<double> &new_x) {
-        vector<double> interpolated_values(new_x.size());
-        for (size_t i = 0; i < new_x.size(); ++i) {
-            double x_value = new_x[i];
-            auto it = lower_bound(x.begin(), x.end(), x_value);
-
-            if (it == x.end() || it == x.begin()) {
-                interpolated_values[i] = 0.0;
-                continue;
-            }
-
-            size_t index = it - x.begin();
-            double x1 = x[index - 1], x2 = x[index];
-            double y1 = y[index - 1], y2 = y[index];
-            interpolated_values[i] = y1 + (y2 - y1) * (x_value - x1) / (x2 - x1);
-        }
-        return interpolated_values;
-    };
-
-    vector<double> interpIntensities = interpolate(wavelengths, data, new_x);
-
     // Zero-padding for FFT
-    size_t fftSize = 1 << static_cast<int>(ceil(log2(dataSize))); // Power of 2 size
+    size_t fftSize = 1 << static_cast<int>(ceil(log2(interpSize))); // Power of 2 size
     vector<double> fftInput(fftSize, 0.0);
     for (size_t i = 0; i < interpIntensities.size(); ++i) {
         fftInput[i] = interpIntensities[i];
     }
 
     // Perform FFT
-    fftw_complex *in = fftw_alloc_complex(fftSize);
-    fftw_complex *out = fftw_alloc_complex(fftSize);
+    fftw_complex* in = fftw_alloc_complex(fftSize);
+    fftw_complex* out = fftw_alloc_complex(fftSize);
 
     for (size_t i = 0; i < fftSize; ++i) {
-        in[i][0] = (i < interpIntensities.size()) ? fftInput[i] : 0.0; // Zero-padding
+        in[i][0] = fftInput[i]; // Zero-padding
         in[i][1] = 0.0;
     }
 
@@ -97,7 +92,9 @@ void ProcessWithPlot::performFFTAndPlot() {
     // Compute magnitudes and frequencies
     vector<double> magnitudes(fftSize / 2);
     vector<double> frequencies(fftSize / 2);
-    double samplingRate = 1.0 / (new_x[1] - new_x[0]); // Sampling rate from wavelengths
+    double dt = 0.001; // Adjust this based on your actual sampling rate
+    double samplingRate = 1.0 / dt; // Sampling rate in Hz
+
 
     for (size_t i = 0; i < fftSize / 2; ++i) {
         magnitudes[i] = sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
@@ -108,18 +105,15 @@ void ProcessWithPlot::performFFTAndPlot() {
     fftw_free(in);
     fftw_free(out);
 
-    // Convert to QVector for plotting
-    QVector<double> qFrequencies;
-    QVector<double> qMagnitudes;
-
-    for (const auto &freq : frequencies) {
-        qFrequencies.append(freq);
-    }
-    for (const auto &mag : magnitudes) {
-        qMagnitudes.append(mag);
-    }
-
+  
     // Plot the FFT results
+    QVector<double> qFrequencies(frequencies.size());
+    QVector<double> qMagnitudes(magnitudes.size());
+
+    // Copy elements from std::vector to QVector
+    std::copy(frequencies.begin(), frequencies.end(), qFrequencies.begin());
+    std::copy(magnitudes.begin(), magnitudes.end(), qMagnitudes.begin());
+
     customPlot->addGraph();
     customPlot->graph(0)->setData(qFrequencies, qMagnitudes);
     customPlot->xAxis->setLabel("Frequency (Hz)");
